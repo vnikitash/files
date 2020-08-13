5e46e0b30726d89f5184c621299b6d91d274708b
Viktor <viktor@stryber.com>
2020-08-13 20:43:19 +0300

    FOPEN

--- index.php
+++ index.php
@@ -1,47 +1,12 @@
 <?php
 
-$deck = [];
 
-function generateDeck()
-{
-    global $deck;
+file_put_contents("index.txt", 'hello', FILE_APPEND);
 
-    $cards = [6,7,8,9,10,'J','Q','K','A'];
-    $suits = ['♡', '♧', '♢', '♤'];
 
-    foreach ($cards as $card) {
-        foreach ($suits as $suit) {
-            $deck[] = $card . $suit;
-        }
-    }
 
-    shuffle($deck);
+$f = fopen('index.txt', 'r');
+
+while ($txt = fread($f, 4)) {
+    echo $txt;
 }
-
-function getNCards(int $numberOfCardsToGive = 6): array
-{
-    global $deck;
-
-    $hand = [];
-
-    if ($numberOfCardsToGive > count($deck)) {
-        $numberOfCardsToGive = count($deck);
-    }
-
-    for ($i = 0; $i < $numberOfCardsToGive; $i++) {
-        $card = array_shift($deck);
-        $hand[] = $card;
-    }
-
-    return $hand;
-}
-
-generateDeck();
-
-print_r($deck);
-$player1 = getNCards(5);
-$player2 = getNCards(5);
-
-print_r($deck);
-print_r($player1);
-print_r($player2);
